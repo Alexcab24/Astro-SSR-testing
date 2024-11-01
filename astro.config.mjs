96bda186178f9abdba180ccc46bc783baf256892
Alexcab24 <sasuke246473@gmail.com>
2024-11-01 15:31:56 -0400

    vercel adapter added

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,12 @@ import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
+import vercel from '@astrojs/vercel/serverless';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	output: 'server',
-	integrations: [mdx(), sitemap()],
+  site: 'https://example.com',
+  output: 'server',
+  integrations: [mdx(), sitemap()],
+  adapter: vercel(),
 });
